b345d25b4a038edced0e0c7590c5918e505e0c41
Demostenes Garcia G <demogar@gmail.com>
2022-10-13 15:05:45 -0500

    feat: removing old .history

--- .history/apps/docs/next.config_20221013093442.js
+++
@@ -1,5 +0,0 @@
-const withTM = require("next-transpile-modules")(["@productplan/atlas"]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-});
--- .history/packages/atlas-react/src/index_20221013093121.js
+++
@@ -1,4 +0,0 @@
-export { default as BasicButton } from './BasicButton';
-export { default as IconButton } from './IconButton';
-export { default as DialogBox } from "./DialogBox";
-export { default as Drawer } from "./Drawer";
