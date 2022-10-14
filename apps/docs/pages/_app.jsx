ddce73486e982d4c30e2b5962f098fc183010562
Demostenes Garcia G <demogar@gmail.com>
2022-10-13 22:43:28 -0500

    feat: add different font

--- apps/docs/pages/_app.jsx
+++ apps/docs/pages/_app.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Navigation from "../common/Navigation";
 import Heading from "../common/Heading";
+import Navigation from "../common/Navigation";
 import componentData from "../data/componentData";
 
 import "./styles.css";
@@ -8,25 +8,15 @@ import "./normalize.css";
 
 const AtlasDocsApp = ({ Component, pageProps }) => {
   return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+    <div>
+      <Heading />
+      <div className="Documentation">
+        <Navigation
+          components={componentData.map((component) => component.name)}
         />
-        <title>ProductPlan Atlas</title>
-      </Head>
-
-      <div>
-        <Heading />
-        <div className="Documentation">
-          <Navigation
-            components={componentData.map((component) => component.name)}
-          />
-          <Component {...pageProps} />
-        </div>
+        <Component {...pageProps} />
       </div>
-    </>
+    </div>
   );
 };
 
---
+++ apps/docs/pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Navigation from "../common/Navigation";
+import Heading from "../common/Heading";
+import componentData from "../data/componentData";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
