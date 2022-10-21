7b1d68d023ea573601af7aa721ddb8522362227a
Demostenes <me@demogar.com>
2022-10-21 17:59:35 -0500

    feat: implement jsx-a11y eslint rule

--- packages/atlas-react/.eslintrc.js
+++ packages/atlas-react/.eslintrc.js
@@ -3,8 +3,13 @@ module.exports = {
     jest: true,
   },
   root: true,
-  plugins: ["prettier"],
-  extends: ["custom", "eslint:recommended", "plugin:react/recommended"],
+  plugins: ["prettier", "jsx-a11y"],
+  extends: [
+    "custom",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
   ignorePatterns: ["node_modules", "dist"],
   rules: {
     "prettier/prettier": "warn",
