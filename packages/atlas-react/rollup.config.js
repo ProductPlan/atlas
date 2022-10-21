d3bece7be2681210ab2c41d71d9f25b6cdd43499
Demostenes Garcia G <demogar@gmail.com>
2022-10-21 09:57:40 -0500

    feat: configuring prettier locally

--- packages/atlas-react/rollup.config.js
+++ packages/atlas-react/rollup.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
