93e09a8e51e3ff88ab63a95769390b68caa77e80
Demostenes Garcia G <demogar@gmail.com>
2022-11-29 06:24:21 -0500

    feat(atlas-react): #183759045 add tokens to css on react components
    add css tokens to react components

--- packages/atlas-react/src/components/BasicButton/BasicButton.js
+++ packages/atlas-react/src/components/BasicButton/BasicButton.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./BasicButton.scss";
 
 if (globalThis?.customElements) {
   import("@productplan/atlas-web-components/dist/lib/BasicButton.js");
